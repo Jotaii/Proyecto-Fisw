25d18aa7ab2e2ae713379809fa168536ecfd152b
Jotaii <juanescalona1994@hotmail.com>
2016-11-19 14:38:02 -0300

    Nueva bd actualizada -> BD-casi-final-sql

--- BD_connection.js
+++ BD_connection.js
@@ -20,7 +20,7 @@ var db = new Db.Adapter({
     server: 'localhost',
     username: 'root',
     password: '1234',
-    database: 'fiswgoku',
+    database: 'LaCaca',
     reconnectTimeout: 2000
 });
 module.exports = db;
